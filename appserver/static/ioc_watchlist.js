0075e8cc76f428a3a333d9973d09bed184e6eba6
Frantisek Kolacek <fkolacek@redhat.com>
2020-03-02 10:32:31 +0100

    Added feature to autodetect type of the indicator

--- appserver/static/ioc_watchlist.js
+++ appserver/static/ioc_watchlist.js
@@ -13,13 +13,27 @@ require([
     return Math.round(new Date(d).getTime() / 1000.0);
   };
 
+  function detect_type(t) {
+    //https://stackoverflow.com/questions/5284147/validating-ipv4-addresses-with-regexp
+    var ipv4 = new RegExp('^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$');
+    //https://stackoverflow.com/questions/53497/regular-expression-that-matches-valid-ipv6-addresses
+    var ipv6 = new RegExp('^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$');
+
+    if(ipv4.test(t))
+      return "ip";
+
+    if(ipv6.test(t))
+      return "ip";
+
+    return "domain";
+  };
+
   function clearForm(){
     $('form *').filter(':input').each(function(){
         $(this).val('');
     });
 
     tok_indicator.val('');
-    tok_type.val('ip');
     tok_start.val(date2string(start));
     tok_end.val(date2string(end));
     tok_reference.val('');
@@ -39,7 +53,6 @@ require([
 
   //Form inputs
   var tok_indicator = $('[name="tok_indicator"]');
-  var tok_type = $('[name="tok_type"]');
   var tok_start = $('[name="tok_start"]');
   var tok_end = $('[name="tok_end"]');
   var tok_reference = $('[name="tok_reference"]');
@@ -58,7 +71,6 @@ require([
     });
 
     tok_indicator.val(values[0]);
-    tok_type.val(values[1]);
     tok_start.val(values[4].replace(/ [0-9\:]+$/, ""));
     tok_end.val(values[5].replace(/ [0-9\:]+$/, ""));
     tok_reference.val(values[6]);
@@ -72,11 +84,12 @@ require([
 
     var s = tok_start.val().replace(/\-/g, '/') + " 00:00:00Z";
     var e = tok_end.val().replace(/\-/g, '/') + " 23:59:59Z";
+    var t = detect_type(tok_indicator.val());
 
     //Create
     if(tok_key.val() == ''){
       tokens.set('ctok_indicator', tok_indicator.val());
-      tokens.set('ctok_type', tok_type.val());
+      tokens.set('ctok_type', t);
       tokens.set('ctok_start', date2epoch(s));
       tokens.set('ctok_end', date2epoch(e));
       tokens.set('ctok_reference', tok_reference.val());
@@ -85,7 +98,7 @@ require([
     //Update
     else{
       tokens.set('utok_indicator', tok_indicator.val());
-      tokens.set('utok_type', tok_type.val());
+      tokens.set('utok_type', t);
       tokens.set('utok_start', date2epoch(s));
       tokens.set('utok_end', date2epoch(e));
       tokens.set('utok_reference', tok_reference.val());
@@ -113,7 +126,6 @@ require([
 
     if(tok_indicator.val() == "$tok_indicator|h$")
       tok_indicator.val('');
-    tok_type.val('ip');
     tok_start.val(date2string(start));
     tok_end.val(date2string(end));
     if(tok_reference.val() == "$tok_reference|h$")
